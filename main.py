2595cd80f675e24d8167355a50076395fd591d24
William Bradford Clark <wclark@redhat.com>
2023-09-18 04:11:59 -0400

    limit block structure to specified layer depth

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ import yaml
 def get_unique_prefixes(state_dict):
     return set(key.split('.'))[0] for key in state_dict.keys())
 
-def analyze_block_structure(state_dict, block_name):
+def analyze_block_structure(state_dict, block_name, depth=None):
     block_keys = [key for key in state_dict if key.startswith(block_name)]
 
     block_structure = {}
     for key in block_keys:
         layers = key.split('.')[len(block_name.split('.')):]
+
+        if depth is not None:
+            layers = layers[:depth]
+
         layer_name = '.'.join(layers)
         block_structure[layer_name] = block_structure.get(layer_name, 0) + 1
 
